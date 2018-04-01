f3e3d73be3b7a5c6dee7cf3c74402b904205354f
Christian Krämer <christian.kraemer@tngtech.com>
2018-04-01 23:55:24 +0200

    removed http - better time sync

--- main.lua
+++ main.lua
@@ -9,20 +9,11 @@ function init()
     station_cfg.pwd=config.PASS
     wifi.sta.config(station_cfg)
 
-    tmr.alarm(1, 5*1000, tmr.ALARM_SINGLE, function()
+    tmr.create():alarm(5*1000, tmr.ALARM_SINGLE, function()
         print(wifi.sta.getip())
+        sntp.sync(nil, nil, nil, 1)
         require("mqttconnection").init_mqtt()
-        require("sntp").sync()
     end)
 end
 
 init()
-
-srv=net.createServer(net.TCP)
-srv:listen(80, function(conn)
-    conn:on("receive", function(sck, payload)
-        require("connection").handle(sck, payload, sensor.getState())
-    end)
-    conn:on("sent", function(sck) sck:close() end)
-end)
-
